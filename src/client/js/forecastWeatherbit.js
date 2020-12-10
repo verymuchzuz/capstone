df24108ebc651bf5c86912fbe042ab726333d15d
zoe <zobalek@deloitte.co.uk>
2020-12-10 10:46:35 +0000

    weatherbit function change

--- src/client/js/forecastWeatherbit.js
+++ src/client/js/forecastWeatherbit.js
@@ -1,9 +1,34 @@
 //this function will get the forecast from Weatherbit
+const weatherUrl = "https://api.weatherbit.io/v2.0/history/daily?";
+const weatherApiKey = cbc67f0272b74d02a4cb5889d6ac91f0;
 
-import {coordinatesGeonames} from "./coordinatesGeonames";
+export let journey = {};
 
-function forecastWeatherbit(){
-    console.log('Weather: ', );
+
+const forecastWeatherbit = async (url, cityInput, apiKey) => {
+    const response = await fetch(weatherUrl + cityInput + weatherApiKey);
+    try {
+        const weatherResponse = await response.json();
+        console.log(weatherResponse);
+        Client.postData('/weatherbitForecast',
+            {
+                day1: weatherResponse.data[0].temp,
+                day2: weatherResponse.data[1].temp,
+                day3: weatherResponse.data[2].temp,
+                day4: weatherResponse.data[3].temp,
+                day5: weatherResponse.data[4].temp,
+            }).then(response => {
+            document.getElementById('day1').innerHTML = weatherResponse.data[0].temp;
+            document.getElementById('day2').innerHTML = weatherResponse.data[1].temp;
+            document.getElementById('day3').innerHTML = weatherResponse.data[2].temp;
+            document.getElementById('day4').innerHTML = weatherResponse.data[3].temp;
+            document.getElementById('day5').innerHTML = weatherResponse.data[4].temp;
+        })
+        return weatherResponse;
+        //error-handling
+    } catch (error) {
+        console.log("Weatherbit error: ", error)
+    }
 }
 
 export {forecastWeatherbit}
