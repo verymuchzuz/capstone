ac444086e47e571cf5868e187c60e05909cda8b7
zoe <zobalek@deloitte.co.uk>
2020-12-24 12:21:18 +0000

    formatting and extra parentheses

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,29 +1,26 @@
 //updating the UI with the data from client
 export let journey = {};
 
-    const updateUI = async journey => {
-        console.log('Updating UI: ', journey);
+const updateUI = async journey => {
+    console.log('Updating UI: ', journey);
 
-        try {
-            const {cityInput, dataFromWeatherbit, pixData} = journey;
+    try {
+        const {cityInput, dataFromWeatherbit, pixData} = journey;
 
-            //identify elements which need to be updated
-            const results__section_city = document.getElementById("results__section_city");
-            const results__section_temp = document.getElementById("results__section_temp");
-            const results__section_image = document.getElementById("results__section_image");
+        //identify elements which need to be updated
+        const results__section_city = document.getElementById("results__section_city");
+        const results__section_temp = document.getElementById("results__section_temp");
+        const results__section_image = document.getElementById("results__section_image");
 
-           //update the screen
-            results__section_city.innerHTML = `Your destination is ${cityInput}`;
-            results__section_temp.innerHTML = `Weather forecast is ${dataFromWeatherbit}`;
-            results__section_image.innerHTML = `${pixData}`;
+        //update the screen
+        results__section_city.innerHTML = `Your destination is ${cityInput}`;
+        results__section_temp.innerHTML = `Weather forecast is ${dataFromWeatherbit}`;
+        results__section_image.innerHTML = `${pixData}`;
 
-            //error-handling
-        } catch (error) {
-            console.log("Update UI error: ", error);
-        }
+        //error-handling
+    } catch (error) {
+        console.log("Update UI error: ", error);
     }
 }
-
-
 
 export {updateUI}
