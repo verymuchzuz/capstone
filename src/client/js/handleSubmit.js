15f33de88fe947a9b5d6d5103827813f0dd49520
zoe <zobalek@deloitte.co.uk>
2021-01-12 12:35:17 +0000

    updating handleSubmit function + refactored imports

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -1,23 +1,20 @@
 //handling all the function to submit the form
 
-import {coordinatesGeonames} from "./coordinatesGeonames";
-import {forecastWeatherbit} from "./forecastWeatherbit";
-import {imagePixabay} from "./imagePixabay"
-import {updateUI, journey} from "./updateUI";
+import {geoWeatherClient} from "./clientInterface";
+import {updateUI} from "./updateUI";
 
+let journey = {};
 
 function handleSubmit(event) {
     event.preventDefault()
 
     const destination = document.getElementById("user_input_destination").value;
-    const date = document.getElementById("start_date").value;
-    const cityInput = document.getElementById("end_date").value;
+    const startDate = document.getElementById("start_date").value;
+    const endDate = document.getElementById("end_date").value;
 
-
-    coordinatesGeonames(destination)
-        .then(() => forecastWeatherbit(date))
-        .then(() => imagePixabay(cityInput))
-        .then(() => updateUI(journey));
+    geoWeatherClient(destination, startDate, endDate).then((response) => {
+        updateUI(response);
+    })
 }
 
 export {handleSubmit}
