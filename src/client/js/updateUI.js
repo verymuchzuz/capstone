2e097855b3b019574899a113ce3ce5942d5784af
zoe <zobalek@deloitte.co.uk>
2021-01-12 12:27:59 +0000

    changing logic in updateUI

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,21 +1,19 @@
 //updating the UI with the data from client
-export let journey = {};
-
-const updateUI = async journey => {
-    console.log('Updating UI: ', journey);
-
+const updateUI = async (journey) => {
     try {
-        const {cityInput, dataFromWeatherbit, pixData} = journey;
+        const {coords, temperature, cityImg} = journey;
 
         //identify elements which need to be updated
         const results__section_city = document.getElementById("results__section_city");
         const results__section_temp = document.getElementById("results__section_temp");
         const results__section_image = document.getElementById("results__section_image");
 
+        const desiredWidth = results__section_image.offsetWidth;
+
         //update the screen
-        results__section_city.innerHTML = `Your destination is ${cityInput}`;
-        results__section_temp.innerHTML = `Weather forecast is ${dataFromWeatherbit}`;
-        results__section_image.innerHTML = `${pixData}`;
+        results__section_city.innerHTML = `Your destination is ${coords.city}`;
+        results__section_temp.innerHTML = `Weather forecast is ${temperature}`;
+        results__section_image.innerHTML = `<img src=\"${cityImg}\" width=\"${desiredWidth}\">`;
 
         //error-handling
     } catch (error) {
